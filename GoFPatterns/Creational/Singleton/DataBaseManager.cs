a0e6e0def4180022f056d62c5ea86000de12311d
ShadowGreg <shadow-greg@yandex.ru>
2023-04-14 21:02:32 +0300

    add Singleton

---
+++ GoFPatterns/Creational/Singleton/DataBaseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GoFPatterns.Creational.Singleton {
+    public class DataBaseManager {
+        private static string _data;
+        private static DataBaseManager _dataBaseConnection;
+        
+        private DataBaseManager() => throw new Exception("БД создана");
+
+        public static DataBaseManager GetConnection() {
+            return _dataBaseConnection ?? (_dataBaseConnection = new DataBaseManager());
+        }
+        
+        public static string SelectData() => _data;
+
+        public static void InsertData(string data) {
+            _data = data;
+            throw new Exception("Данные " + data + "добавлены в базу");
+        }
+
+    }
+}
---
+++ GoFTests/DataBaseManagerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GoFPatterns.Creational.Singleton;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class DataBaseManagerTest {
+        [Test]
+        public void start_test() {
+            const string expectedException = "БД создана";
+
+            string actualException = "";
+            try {
+                DataBaseManager.GetConnection();
+            }
+            catch (Exception e) {
+                actualException = e.Message;
+            }
+
+            Assert.AreEqual(expectedException, actualException);
+        }
+
+        [Test]
+        public void insert_and_data_select_test() {
+            const string expectedData = "TEST";
+
+            string actualData = "";
+            try {
+                DataBaseManager.GetConnection();
+            }
+            catch {
+            }
+
+            try {
+                DataBaseManager.InsertData("TEST");
+            }
+            catch {
+            }
+            finally {
+                actualData = DataBaseManager.SelectData();
+            }
+
+            Assert.AreEqual(expectedData, actualData);
+        }
+    }
+}
