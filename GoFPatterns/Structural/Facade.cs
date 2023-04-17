18d14200eab0d3bec39c3ccb941f304f2955419a
ShadowGreg <shadow-greg@yandex.ru>
2023-04-17 10:38:40 +0300

    add Facade

---
+++ GoFPatterns/Structural/Facade.cs
@@ -0,0 +1,28 @@
+﻿namespace GoFPatterns.Structural {
+    
+    public class OperationA {
+        public string SomeActionA() => "Выполнена операция класса А";
+    }
+    
+    public class OperationB {
+        public string SomeActionB() => "Выполнена операция класса B";
+    }
+    
+    public class OperationC {
+        public string SomeActionC() => "Выполнена операция класса C";
+    }
+
+    public class Facade {
+        private readonly OperationA _a;
+        private readonly OperationB _b;
+        private readonly OperationC _c;
+
+        public Facade(OperationA a, OperationB b, OperationC c) {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+        public string FirstDifficultyOperation() => _a.SomeActionA()+_b.SomeActionB();
+        public string SecondDifficultyOperation() => _a.SomeActionA() + _b.SomeActionB() + _c.SomeActionC();
+    }
+}
---
+++ GoFTests/FacadeTest.cs
@@ -0,0 +1,17 @@
+﻿using GoFPatterns.Structural;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class FacadeTest {
+        private readonly Facade _facade = new Facade(new OperationA(), new OperationB(), new OperationC());
+
+        [Test]
+        public void some_command_from_facade_are_notEqual() {
+            string fistOperation = _facade.FirstDifficultyOperation();
+            string secondOperation = _facade.SecondDifficultyOperation();
+            
+            Assert.AreNotEqual(fistOperation,secondOperation);
+        }
+    }
+}
