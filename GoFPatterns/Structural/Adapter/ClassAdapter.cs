ba4f20ec13d0f8bd86adb94304e8372f8bbc7df2
ShadowGreg <shadow-greg@yandex.ru>
2023-04-17 08:26:34 +0300

    add Adapter с расширением функции класса

---
+++ GoFPatterns/Structural/Adapter/ClassAdapter.cs
@@ -0,0 +1,31 @@
+﻿namespace GoFPatterns.Structural {
+    public interface ICScales {
+        float GetWeight();
+        string Adjust();
+    }
+
+    public class CRussianScales: ICScales {
+        private readonly float currentWeight;
+        public CRussianScales(float currentWeight) => this.currentWeight = currentWeight;
+        public float GetWeight() => this.currentWeight;
+        public string Adjust() => "регулировка русских весов";
+    }
+
+    public class BritanScales {
+        private readonly float currentWeight;
+        public BritanScales(float currentWeight) => this.currentWeight = currentWeight;
+        public float GetWeight() => this.currentWeight;
+        public string Adjust() => "регулировка британских весов";
+    }
+
+    public class AdapterForBritanScales: BritanScales, ICScales{
+        public AdapterForBritanScales(float cw): base(cw) { }
+
+        float ICScales.GetWeight() => base.GetWeight() * 0.453f;
+        
+        string ICScales.Adjust() => base.Adjust()+"\n британские весы адаптированны под российский вес";
+        
+    }
+
+    public class ClassAdapter { }
+}
---
+++ GoFTests/ClassAdapterTest.cs
@@ -0,0 +1,15 @@
+﻿using GoFPatterns.Structural;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class ClassAdapterTest {
+        private ICScales rCScales = new CRussianScales(55.0f);
+        private ICScales bCScales = new AdapterForBritanScales(55.0f);
+
+        [Test]
+        public void the_scales_are_different_between_rScales_bScales() {
+            Assert.AreNotEqual(rCScales,bCScales);
+        }
+    }
+}
