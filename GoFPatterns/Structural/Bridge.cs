8fd3683f3a4952aec173edccb7909bc7431cf9e8
ShadowGreg <shadow-greg@yandex.ru>
2023-04-17 10:00:29 +0300

    add Bridge

---
+++ GoFPatterns/Structural/Bridge.cs
@@ -0,0 +1,35 @@
+﻿namespace GoFPatterns.Structural {
+    public interface IControlImpl {
+        string DrawControl();
+    }
+
+    public abstract class ImplControl {
+        protected IControlImpl _implementation;
+        protected ImplControl(IControlImpl impl) => _implementation = impl;
+        public abstract string Draw();
+    }
+
+    public class WindowsControl: IControlImpl {
+        public string DrawControl() => "нарисован для Windows";
+    }
+
+    public class WebControl: IControlImpl {
+        public string DrawControl() => "нарисован для Web приложения";
+    }
+
+    public class Button: ImplControl {
+        public Button(IControlImpl imp): base(imp) { }
+
+        public override string Draw() =>
+            "Элменет кнопка " + _implementation.DrawControl();
+    }
+
+    public class TextBox: ImplControl {
+        public TextBox(IControlImpl imp): base(imp) { }
+
+        public override string Draw() =>
+            "Элменет текст бокс " + _implementation.DrawControl();
+    }
+
+    public class Bridge { }
+}
---
+++ GoFTests/BridgeTest.cs
@@ -0,0 +1,38 @@
+﻿using GoFPatterns.Structural;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class BridgeTest {
+        private readonly WindowsControl _winC = new WindowsControl();
+        private readonly WebControl _webC = new WebControl();
+
+        [Test]
+        public void text_from_button_win_and_web_are_not_equal() {
+            Button winButton = new Button(_winC);
+            Button webButton = new Button(_webC);
+            
+            Assert.AreNotEqual(winButton.Draw(),webButton.Draw());
+        }
+        
+        [Test]
+        public void textBox_win_text() {
+            TextBox winTextBox = new TextBox(_winC);
+            const string expectedText = "Элменет текст бокс нарисован для Windows";
+            
+            string actualText = winTextBox.Draw();
+            
+            Assert.AreEqual(expectedText,actualText);
+        }
+        
+        [Test]
+        public void textBox_web_text() {
+            TextBox winTextBox = new TextBox(_webC);
+            const string expectedText = "Элменет текст бокс нарисован для Web приложения";
+            
+            string actualText = winTextBox.Draw();
+            
+            Assert.AreEqual(expectedText,actualText);
+        }
+    }
+}
