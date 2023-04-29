cd17b9993f013ae905ec28fc4cf7c4fc6d3a7fcf
ShadowGreg <shadow-greg@yandex.ru>
2023-04-29 19:16:49 +0300

    add Proxy

---
+++ GoFPatterns/Structural/Proxy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace GoFPatterns.Structural {
+    public interface ISite {
+        string GetPage(int num);
+    }
+
+    public class Site: ISite {
+        public string GetPage(int num) => "Это страница " + num;
+    }
+
+    public class SiteProxy: ISite {
+        private readonly ISite _site;
+        private Dictionary<int, string> cache;
+
+        public SiteProxy(ISite site) {
+            _site = site;
+            cache = new Dictionary<int, string>();
+        }
+
+        public string GetPage(int num) {
+            string page;
+            if (cache.ContainsKey(num)) {
+                page = "из кэша: " + cache[num];
+            }
+            else {
+                page = _site.GetPage(num);
+                cache.Add(num, page);
+            }
+
+            return page;
+        }
+    }
+}
---
+++ GoFTests/ProxyTests.cs
@@ -0,0 +1,38 @@
+﻿using GoFPatterns.Structural;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class ProxyTests {
+        private ISite mySite = new SiteProxy(new Site());
+        
+        [Test]
+        public void Extract_Pages_Test() {
+            // Arrange
+            string expected =
+                "Это страница 1";
+
+            // Act
+
+
+            // Assert
+            string actual = mySite.GetPage(1);
+            Assert.AreEqual(expected, actual);
+        }
+        
+                
+        [Test]
+        public void Pages_Сaching_Test() {
+            // Arrange
+            string expected =
+                "из кэша: Это страница 1";
+
+            // Act
+            Extract_Pages_Test();
+
+            // Assert
+            string actual = mySite.GetPage(1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
