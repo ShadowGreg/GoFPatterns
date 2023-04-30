6e53e488ccca394299e199f9a56a210b0e287f7c
ShadowGreg <shadow-greg@yandex.ru>
2023-04-30 12:29:40 +0300

    add Command

---
+++ GoFPatterns/Behavioral/Command.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace GoFPatterns.Behavioral {
+    public interface ICommand {
+        string Positive();
+        string Negative();
+    }
+
+    public class Conveyor {
+        public string On() => "Конвейер запущен. ";
+        public string Off() => "Конвейер остановлен. ";
+        public string IncreaseSpeed() => "Скорость конвейера увеличина. ";
+        public string ReduceSpeed() => "Скорость конвейера понижена. ";
+    }
+
+    public class EnableDisableButtons: ICommand {
+        private readonly Conveyor _conveyor;
+        public EnableDisableButtons(Conveyor conveyor) => _conveyor = conveyor;
+
+        public string Positive() => _conveyor.On();
+        public string Negative() => _conveyor.Off();
+    }
+
+    public class ZoomButtonsDecreaseSpeed: ICommand {
+        private readonly Conveyor _conveyor;
+        public ZoomButtonsDecreaseSpeed(Conveyor conveyor) => _conveyor = conveyor;
+
+        public string Positive() => _conveyor.IncreaseSpeed();
+        public string Negative() => _conveyor.ReduceSpeed();
+    }
+
+    public class СonveyorControlPanel {
+        private readonly List<ICommand> _commands;
+        private readonly Stack<ICommand> _history;
+
+        public СonveyorControlPanel() {
+            _commands = new List<ICommand>() { null, null };
+            _history = new Stack<ICommand>();
+        }
+
+        public void SetCommand(int numButton, ICommand command) => _commands[numButton] = command;
+
+        public string PressOn(int button) {
+            _history.Push(_commands[button]);
+            return _commands[button].Positive();
+        }
+
+        public string PressCancel() {
+            return _history.Count != 0 ? _history.Pop().Negative() : string.Empty;
+        }
+    }
+
+    public class Command { }
+}
---
+++ GoFTests/CommandTests.cs
@@ -0,0 +1,45 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class CommandTests {
+        private static readonly Conveyor _conveyor = new Conveyor();
+        private static readonly СonveyorControlPanel multipult = new СonveyorControlPanel();
+
+        private static void SetUp() {
+            multipult.SetCommand(0, new EnableDisableButtons(_conveyor));
+            multipult.SetCommand(1, new ZoomButtonsDecreaseSpeed(_conveyor));
+        }
+
+
+        [Test]
+        public void Pipeline_Enable_Test() {
+            // Arrange
+            string expected =
+                "Конвейер запущен. Скорость конвейера увеличина. ";
+
+            // Act
+            SetUp();
+            string actual = multipult.PressOn(0) + multipult.PressOn(1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Undo_Commands_Test() {
+            // Arrange
+            string expected =
+                "Конвейер запущен. Скорость конвейера увеличина. Скорость конвейера понижена. Конвейер остановлен. ";
+
+            // Act
+            SetUp();
+            string actual = multipult.PressOn(0) + multipult.PressOn(1) + multipult.PressCancel() +
+                            multipult.PressCancel();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
