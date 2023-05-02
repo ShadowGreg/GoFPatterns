a9bffc5ef485c3afc20d93b2098fed3128b3f903
ShadowGreg <shadow-greg@yandex.ru>
2023-05-02 08:32:33 +0300

    add Iterator.cs and IteratorTests.cs

---
+++ GoFPatterns/Behavioral/Iterator.cs
@@ -0,0 +1,66 @@
+﻿using System.Dynamic;
+
+namespace GoFPatterns.Behavioral {
+    public class DataStack {
+        private readonly int[] _items = new int[10];
+        private int _lenght;
+        public DataStack() => _lenght = -1;
+
+        public DataStack(DataStack inDataStack) {
+            _items = inDataStack._items;
+            _lenght = inDataStack._lenght;
+        }
+
+        public int[] Items => _items;
+        public int Lenght => _lenght;
+
+        public void Push(int value) => _items[++_lenght] = value;
+        public int Pop() => _items[_lenght--];
+
+        public static bool operator ==(DataStack firstItem, DataStack secondItem) {
+            StackIterator it1 = new StackIterator(firstItem),
+                it2 = new StackIterator(secondItem);
+            while (it1.IsEnd() || it2.IsEnd()) {
+                if (it1.Get() != it2.Get()) break;
+                it1++;
+                it2++;
+            }
+            return !it1.IsEnd() && !it2.IsEnd();
+        }
+
+        public static bool operator !=(DataStack firstItem, DataStack secondItem) {
+            StackIterator it1 = new StackIterator(firstItem),
+                it2 = new StackIterator(secondItem);
+            while (it1.IsEnd() || it2.IsEnd()) {
+                if (it1.Get() != it2.Get()) break;
+                it1++;
+                it2++;
+            }
+
+            return !(!it1.IsEnd() && !it2.IsEnd());
+        }
+
+        public class StackIterator {
+            private DataStack _stack;
+            private int _index;
+
+            public StackIterator(DataStack myStack) {
+                _stack = myStack;
+                _index = 0;
+            }
+
+            public static StackIterator operator ++(StackIterator s) {
+                s._index++;
+                return s;
+            }
+
+            public int Get() {
+                return _index < _stack.Lenght ? _stack.Items[_index] : 0;
+            }
+
+            public bool IsEnd() => _index != _stack.Lenght + 1;
+        }
+    }
+
+    public class Iterator { }
+}
---
+++ GoFTests/IteratorTests.cs
@@ -0,0 +1,57 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class IteratorTests {
+        [Test]
+        public void Comparison_Of_Two_Unequal_Stacks_Test() {
+            // Arrange
+            DataStack firstStack = new DataStack();
+            DataStack secondStack = new DataStack();
+
+            // Act
+            for (int i = 0; i < 10; i++) {
+                firstStack.Push(i);
+            }
+
+            // Assert
+            Assert.False(firstStack == secondStack);
+        }
+
+        [Test]
+        public void Copying_From_One_Stack_To_Another_Test() {
+            // Arrange
+            DataStack firstStack = new DataStack();
+
+            // Act
+            for (int i = 0; i < 10; i++) {
+                firstStack.Push(i);
+            }
+
+            DataStack secondStack = new DataStack(firstStack);
+
+
+            // Assert
+            Assert.True(firstStack == secondStack);
+        }
+        
+        [Test]
+        public void Test_Removal_Items_From_The_Stack_Test() {
+            // Arrange
+            DataStack firstStack = new DataStack();
+
+            // Act
+            for (int i = 0; i < 10; i++) {
+                firstStack.Push(i);
+            }
+
+            DataStack secondStack = new DataStack(firstStack);
+            firstStack.Pop();
+
+
+            // Assert
+            Assert.True(firstStack != secondStack);
+        }
+    }
+}
