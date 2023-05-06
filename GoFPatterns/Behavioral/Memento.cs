cfe1bf10e311052155a8865547eac9ce91ad22c6
ShadowGreg <shadow-greg@yandex.ru>
2023-05-06 08:58:19 +0300

    Add Memento

---
+++ GoFPatterns/Behavioral/Memento.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace GoFPatterns.Behavioral {
+    public interface IMemento {
+        double GetGold();
+        double GetSilver();
+    }
+
+    public class ExchangeMemento: IMemento {
+        private double _gold;
+        private double _silver;
+
+        public ExchangeMemento(double gold, double silver) {
+            _gold = gold;
+            _silver = silver;
+        }
+
+        public double GetGold() => _gold;
+        public double GetSilver() => _silver;
+    }
+
+    public class Exchange {
+        private double _gold;
+        private double _silver;
+
+        public Exchange(double gold, double silver) {
+            _gold = gold;
+            _silver = silver;
+        }
+
+        public string GetGold() => "Количество золота: " + _gold;
+        public string GetSilver() => "Количество серебра: " + _silver;
+
+        public void Sell() {
+            if (_gold > 0) _gold--;
+        }
+
+        public void Buy() => _silver++;
+
+        public ExchangeMemento Save() => new ExchangeMemento(_gold, _silver);
+
+        public void Restore(IMemento exchangeMemento) {
+            _gold = exchangeMemento.GetGold();
+            _silver = exchangeMemento.GetSilver();
+        }
+    }
+
+    public class Memory {
+        private Stack<IMemento> _history;
+        private Exchange _exchange;
+
+        public Memory(Exchange exchange) {
+            _exchange = exchange;
+            _history = new Stack<IMemento>();
+        }
+
+        public void Backup() => _history.Push(_exchange.Save());
+
+        public void Undo() {
+            if (_history.Count == 0) return;
+            _exchange.Restore(_history.Pop());
+        }
+    }
+
+    public class Memento { }
+}
---
+++ GoFTests/MementoTests.cs
@@ -0,0 +1,62 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class MementoTests {
+        private Exchange _exchange;
+        private Memory _memory;
+
+        [SetUp]
+        public void SetUp() {
+            _exchange = new Exchange(10.5, 10.7);
+            _memory = new Memory(_exchange);
+        }
+
+        [Test]
+        public void Availability_Of_Metals_On_The_Stock_Exchange_Test() {
+            // Arrange
+            SetUp();
+            const string expected = "Количество золота: 10,5 Количество серебра: 10,7";
+
+            // Act
+
+            // Assert
+            string actual = _exchange.GetGold() + " " + _exchange.GetSilver();
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void Sale_And_Purchase_Of_Metals_Test() {
+            // Arrange
+            Availability_Of_Metals_On_The_Stock_Exchange_Test();
+            const string expected = "Количество золота: 9,5 Количество серебра: 11,7";
+
+            // Act
+            _exchange.Sell();
+            _exchange.Buy();
+
+            // Assert
+            string actual = _exchange.GetGold() + " " + _exchange.GetSilver();
+            Assert.AreEqual(expected, actual);
+        }
+        
+                
+        [Test]
+        public void Сonservation_Test() {
+            // Arrange
+            Sale_And_Purchase_Of_Metals_Test();
+            const string expected = "Количество золота: 9,5 Количество серебра: 11,7";
+
+            // Act
+            _memory.Backup();
+            _exchange.Sell();
+            _exchange.Buy();
+            _memory.Undo();
+
+            // Assert
+            string actual = _exchange.GetGold() + " " + _exchange.GetSilver();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
