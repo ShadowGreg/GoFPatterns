302f80f536bcaf7b52b49eb9ef09f6b958085398
ShadowGreg <shadow-greg@yandex.ru>
2023-05-06 10:13:19 +0300

    add Observer

---
+++ GoFPatterns/Behavioral/Observer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GoFPatterns.Behavioral {
+    public interface IObserver {
+        string Update(double price);
+        string ToString();
+    }
+
+    public interface IObservable {
+        string AddObserver(IObserver observer);
+        string RemoveObserver(IObserver observer);
+        string Notify();
+    }
+
+    public class Product: IObservable {
+        private readonly List<IObserver> _observers;
+        private double _price;
+
+        public Product(double price) {
+            _price = price;
+            _observers = new List<IObserver>();
+        }
+
+        public string ChangePrice(double newPrice) {
+            _price = newPrice;
+            return "Цена изменилась и стала " + newPrice + Notify();
+        }
+
+
+        public string AddObserver(IObserver observer) {
+            _observers.Add(observer);
+            return "За товаром наблюдает: " + observer.ToString();
+        }
+
+        public string RemoveObserver(IObserver observer) {
+            _observers.Remove(observer);
+            return observer.ToString() + " болше не следит за товаром";
+        }
+
+        public string Notify() {
+            string message = string.Empty;
+            foreach (IObserver observer in _observers.ToList()) {
+                message += observer.Update(_price);
+                message += " " + observer.ToString();
+            }
+
+            return message + " предупреждены об измении цены";
+        }
+
+        public override string ToString() => "Некий товар";
+    }
+
+    public class Wholesale: IObserver {
+        private readonly IObservable _product;
+
+        public Wholesale(IObservable product) {
+            _product = product;
+            product.AddObserver(this);
+        }
+
+        public string Update(double price) {
+            if (!(price < 300)) return "Не подходящая цена на " + _product + " для " + this;
+            _product.RemoveObserver(this);
+            return this + " закупил " + _product;
+        }
+
+        public override string ToString() {
+            return "Оптовик";
+        }
+    }
+
+    public class Buyer: IObserver {
+        private readonly IObservable _product;
+
+        public Buyer(IObservable product) {
+            _product = product;
+            product.AddObserver(this);
+        }
+
+        public string Update(double price) {
+            if (!(price < 350)) return "Не подходящая цена на " + _product + " для " + this;
+            _product.RemoveObserver(this);
+            return this + " закупил " + _product;
+        }
+
+        public override string ToString() {
+            return "Покупатель";
+        }
+    }
+
+    public class Observer { }
+}
---
+++ GoFTests/ObserverTests.cs
@@ -0,0 +1,48 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class ObserverTests {
+        private Product _product;
+        private Wholesale _wholesale;
+        private Buyer _buyer;
+
+        [SetUp]
+        public void SetUp() {
+            _product = new Product(400.99);
+            _wholesale = new Wholesale(_product);
+            _buyer = new Buyer(_product);
+        }
+
+        [Test]
+        public void Warning_The_Entire_Chain_Of_Observers_About_Changes_In_The_Price_Of_Goods_Test() {
+            // Arrange
+            SetUp();
+            const string expected =
+                "Цена изменилась и стала 399,99Не подходящая цена на Некий товар для Оптовик ОптовикНе подходящая цена на Некий товар для Покупатель Покупатель предупреждены об измении цены";
+
+            // Act
+            string actual = _product.ChangePrice(399.99);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void Purchase_Of_Goods_By_The_Buyer_And_Seller_Test() {
+            // Arrange
+            SetUp();
+            const string expected =
+                "Цена изменилась и стала 349,99Не подходящая цена на Некий товар для Оптовик ОптовикПокупатель закупил Некий товар Покупатель предупреждены об измении ценыЦена изменилась и стала 210,34Оптовик закупил Некий товар Оптовик предупреждены об измении цены";
+
+            // Act
+            string actual = _product.ChangePrice(349.99);
+            actual += _product.ChangePrice(210.34);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
