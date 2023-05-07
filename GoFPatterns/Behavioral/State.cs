3edebd60c767192cdd6876ed80161bb783e25978
ShadowGreg <shadow-greg@yandex.ru>
2023-05-07 09:19:55 +0300

    add State

---
+++ GoFPatterns/Behavioral/State.cs
@@ -0,0 +1,65 @@
+﻿namespace GoFPatterns.Behavioral {
+    public abstract class State {
+        protected TrafficLight _trafficLight;
+
+        public TrafficLight TrafficLight
+        {
+            set => _trafficLight = value;
+        }
+
+        public abstract string NextState();
+        public abstract string PreviousState();
+    }
+
+    public class TrafficLight {
+        private State _state;
+        public TrafficLight(State state) => SetState(state);
+
+        public string SetState(State state) {
+            _state = state;
+            _state.TrafficLight = this;
+            return "Установлено состояние " + _state;
+        }
+
+        public string NextState() {
+            return "Установлено состояние " + _state.NextState();
+        }
+        public string PreviousState() {
+            return "Установлено состояние " + _state.PreviousState();
+        }
+    }
+
+    public class GreenState : State{
+        public override string NextState() {
+            _trafficLight.SetState(new YellowState());
+            return "Состояние переведено из зелёного в жёлтый ";
+        }
+
+        public override string PreviousState() {
+            return "Состяние преведено в зелёный ";
+        }
+    }
+    
+    public class YellowState : State{
+        public override string NextState() {
+            _trafficLight.SetState(new ReadState());
+            return "Состояние переведено из жёлтого в красный ";
+        }
+
+        public override string PreviousState() {
+            _trafficLight.SetState(new GreenState());
+            return "Состяние преведено из жёлтого в зелёный ";
+        }
+    }
+    
+    public class ReadState : State{
+        public override string NextState() {
+            return "Состояние красный ";
+        }
+
+        public override string PreviousState() {
+            _trafficLight.SetState(new YellowState());
+            return "Состяние преведено в жёлтый ";
+        }
+    }
+}
---
+++ GoFTests/StateTests.cs
@@ -0,0 +1,69 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class StateTests {
+        private TrafficLight _trafficLight;
+
+        [SetUp]
+        public void SetUp() {
+            _trafficLight = new TrafficLight(new YellowState());
+        }
+
+        [Test]
+        public void Switch_The_Traffic_Light_To_The_Next_Signal_In_Order_Test() {
+            // Arrange
+            const string expected = "Установлено состояние Состояние переведено из жёлтого в красный ";
+
+            // Act
+            string actual = _trafficLight.NextState();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        
+        [Test]
+        public void We_Make_Sure_That_The_Lower_State_Is_Attached_Test() {
+            // Arrange
+            const string expected = "Установлено состояние Состояние красный ";
+
+            // Act
+            _trafficLight.NextState();
+            _trafficLight.NextState();
+            _trafficLight.NextState();
+            string actual = _trafficLight.NextState();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Switch_The_Traffic_Light_To_The_First_Signal_In_Order_Test() {
+            // Arrange
+            const string expected = "Установлено состояние Состяние преведено из жёлтого в зелёный ";
+
+            // Act
+            string actual = _trafficLight.PreviousState();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void We_Make_Sure_That_The_Upper_State_Is_Attached__Test() {
+            // Arrange
+            const string expected = "Установлено состояние Состяние преведено в зелёный ";
+
+            // Act
+            _trafficLight.PreviousState();
+            _trafficLight.PreviousState();
+            _trafficLight.PreviousState();
+            string actual = _trafficLight.PreviousState();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
