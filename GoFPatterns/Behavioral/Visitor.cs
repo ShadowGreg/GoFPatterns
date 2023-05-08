0cbe07bd8b9f3506c508378f871930facda5df87
ShadowGreg <shadow-greg@yandex.ru>
2023-05-08 14:49:56 +0300

    add Visitor

---
+++ GoFPatterns/Behavioral/Visitor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GoFPatterns.Behavioral {
+    public interface IVisitor {
+        void visit(Zoo zoo);
+        void visit(Cinema cinema);
+        void visit(Circus сircus);
+        void visit(Park park);
+    }
+
+    public interface IPlace {
+        void Accept(IVisitor v);
+    }
+
+    public class Zoo: IPlace {
+        public void Accept(IVisitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class Cinema: IPlace {
+        public void Accept(IVisitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class Circus: IPlace {
+        public void Accept(IVisitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class Park: IPlace {
+        public void Accept(IVisitor v) {
+            v.visit(this);
+        }
+    }
+
+    public class HolidayMaker: IVisitor {
+        public string Msg = string.Empty;
+        public void visit(Zoo zoo) {
+            Msg =  "В зоопарке я увидел слона. ";
+        }
+
+        public void visit(Cinema cinema) {
+            Msg = "В кинотеатре мы посомтрели фильм Армагедон. ";
+        }
+
+        public void visit(Circus сircus) {
+            Msg =  "В цирке мы увидели акробата. ";
+        }
+
+        public void visit(Park park) {
+            Msg =  "В парке мы увидели акробата. ";
+        }
+    }
+}
---
+++ GoFTests/VisitorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class VisitorTests {
+                                        
+        [Test]
+        public void When_The_Visitor_Visited_All_The_Interesting_Places_In_The_City_Test() {
+            // Arrange
+            List<IPlace> _placesies = new List<IPlace>(){new Zoo(),new Cinema(),new Circus(), new Park()};
+            const string expected = "В зоопарке я увидел слона. В кинотеатре мы посомтрели фильм Армагедон. В цирке мы увидели акробата. В парке мы увидели акробата. ";
+
+            // Act
+            string actual = string.Empty;
+            foreach (IPlace place in _placesies) {
+                HolidayMaker visitor = new HolidayMaker();
+                place.Accept(visitor);
+                actual += visitor.Msg;
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
