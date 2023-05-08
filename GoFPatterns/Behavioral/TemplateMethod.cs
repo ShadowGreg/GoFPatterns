baded12c6317c8332f416a7aedc671e5b6e7ab8a
ShadowGreg <shadow-greg@yandex.ru>
2023-05-08 11:47:50 +0300

    add TemplateMethod

---
+++ GoFPatterns/Behavioral/TemplateMethod.cs
@@ -0,0 +1,26 @@
+﻿namespace GoFPatterns.Behavioral {
+    public abstract class Transmitter {
+        protected virtual string VoiceRecord() => "Запись фрагмента речи";
+        protected virtual string Simpling() => string.Empty;
+        protected virtual string Digitization() => string.Empty;
+        protected virtual string Modulation() => string.Empty;
+        protected virtual string Transmission() => "Передача сигнала по радиоканалу";
+
+        public string ProcessStart() {
+            string _msg = VoiceRecord() + Simpling() + Digitization() + Modulation() +
+                          Transmission();
+            return _msg;
+        }
+    }
+
+    public class AnalogTransmitter: Transmitter {
+        protected override string Modulation() => "Модуляция аналового сигнала";
+    }
+
+    public class DigitalTransmitter: Transmitter {
+        protected override string Simpling() => "Модуляция аналового сигнала";
+        protected override string Digitization() => "Модуляция аналового сигнала";
+        protected override string Modulation() => "Модуляция аналового сигнала";
+    }
+    public class TemplateMethod { }
+}
---
+++ GoFTests/TemplateMethodTests.cs
@@ -0,0 +1,34 @@
+﻿using GoFPatterns.Behavioral;
+using NUnit.Framework;
+
+namespace GoFTests {
+    [TestFixture]
+    public class TemplateMethodTests {
+                                
+        [Test]
+        public void AnalogTransmitter_Test() {
+            // Arrange
+            Transmitter _transmitter = new AnalogTransmitter();
+            const string expected = "Запись фрагмента речиМодуляция аналового сигналаПередача сигнала по радиоканалу";
+
+            // Act
+            string actual = _transmitter.ProcessStart();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void DigitalTransmitter_Test() {
+            // Arrange
+            Transmitter _transmitter = new DigitalTransmitter();
+            const string expected = "Запись фрагмента речиМодуляция аналового сигналаМодуляция аналового сигналаМодуляция аналового сигналаПередача сигнала по радиоканалу";
+
+            // Act
+            string actual = _transmitter.ProcessStart();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
